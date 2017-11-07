c2e2d512bc87711d8470a8d04c58cc9d4aeb880e
Djelali DINAUT <ddinaut@e1r11p20.42.fr>
2017-11-07 16:45:18 +0100

    done

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 16:04:44 by ddinaut           #+#    #+#             */
+/*   Updated: 2017/11/07 16:12:35 by ddinaut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "includes/printf.h"
 
@@ -8,11 +20,9 @@ int main(void)
 
 	c = 42000450;
 	cc = 0xFFEF;
-//	ft_printf("1-> %x et %X", c, cc);
-	ft_printf("1-> %c et %c", 'a', 'b');
+	ft_printf("1-> %x et %X", c, cc);
 	printf("\n");
-	printf("1-> %c et %c", 'a', 'b');
-//	printf("2-> %x et %X", c, cc);
+	printf("2-> %x et %X", c, cc);
 	printf("\n");
 	return (0);
 }
--- srcs/conv/do_x.c
+++ srcs/conv/do_x.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_x.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 16:04:36 by ddinaut           #+#    #+#             */
+/*   Updated: 2017/11/07 16:45:03 by ddinaut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "printf.h"
 
 static int	ft_len(int i, int base)
@@ -33,6 +45,7 @@ void	do_x(va_list arg, char c)
 	unsigned int	i;
 	char			*str;
 
+	str = NULL;
 	if (c == 'x')
 	{
 		i = va_arg(arg, int);
@@ -43,7 +56,7 @@ void	do_x(va_list arg, char c)
 		i = va_arg(arg, int);
 		str = ft_convert(i, 16, "0123456789ABCDEF");
 	}
-	if (str)
+	if (str != NULL)
 	{
 		ft_putstr(str);
 		free(str);
